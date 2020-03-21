0c71febddc2777ebbd833d190cd3e46fc278711d
Roman <7967717@gmail.com>
2020-03-21 17:01:05 +0200

    added schema

--- Spring/springJpa/src/main/java/io/rr/springJpa/UserRecord.java
+++ Spring/springJpa/src/main/java/io/rr/springJpa/UserRecord.java
@@ -2,13 +2,11 @@ package io.rr.springJpa;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "USERRECORDS")
 public class UserRecord {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
---
+++ Spring/springJpa/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO UserRecords VALUES (1, 'Bob', 'UK');
+INSERT INTO UserRecords VALUES (2, 'Mac', 'UK');
+INSERT INTO UserRecords VALUES (3, 'John', 'US');
